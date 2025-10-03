bb38f2d554fd5b685844f5ed9454f0706f382713
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-03 12:33:04 +0300

    refactor: make visitorId optional in ClickRequest DTO

--- src/callclick/dto/callclick.dto.ts
+++ src/callclick/dto/callclick.dto.ts
@@ -1,12 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
 
 export class ClickRequest {
   @ApiProperty({
     example: 'visitor-123',
     description: 'Уникальный идентификатор посетителя',
   })
-  @IsString()
-  @IsNotEmpty()
-  visitorId: string;
+  visitorId?: string;
 }
