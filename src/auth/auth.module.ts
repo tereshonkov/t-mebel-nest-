2fc090788ec0f0deec5b8fb5c02e169ee00abb73
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-03 12:27:48 +0300

    feat: export AuthService from AuthModule for external usage

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -20,5 +20,6 @@ import { JwtStrategy } from './strategies/jwt.strategy';
   ],
   controllers: [AuthController],
   providers: [AuthService, JwtStrategy],
+  exports: [AuthService],
 })
 export class AuthModule {}
