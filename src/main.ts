f818c1fa4182764dd8af79411276d65144834d60
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-07 16:46:55 +0300

    fix: set trust proxy for improved request handling

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,14 @@ import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { ValidationPipe } from '@nestjs/common';
 import { setupSwagger } from './utils/swagger.utils';
+import express from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const expressApp = app.getHttpAdapter().getInstance() as express.Express;
+  expressApp.set('trust proxy', 1);
+
   app.enableCors({
     origin: ['https://www.t-mebel.com.ua', 'http://localhost:3000'],
     credentials: true,
