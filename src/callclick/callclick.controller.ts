080c3da035b319b1f5e407ad3623df911829d2f3
tereshonkov <tereshonkov.dima@gmail.com>
2025-10-08 12:59:31 -0700

    fix: simplify recordClick method by removing unnecessary parameters

--- src/callclick/callclick.controller.ts
+++ src/callclick/callclick.controller.ts
@@ -1,23 +1,15 @@
-import { Controller, Req } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 import { CallclickService } from './callclick.service';
 import { Post, Body, Get, Headers } from '@nestjs/common';
-import { ClickRequest } from './dto/callclick.dto';
 import { CallClick } from '@prisma/client';
-import type { Request } from 'express';
 
 @Controller('callclick')
 export class CallclickController {
   constructor(private readonly callclickService: CallclickService) {}
 
   @Post('record')
-  async recordClick(
-    @Body() dto: ClickRequest,
-    @Req() req: Request,
-  ): Promise<CallClick> {
-    const ip =
-      req.ip || req.headers['x-forwarded-for']?.toString() || 'unknown';
-    const userAgent = req.headers['user-agent'] || 'unknown';
-    return await this.callclickService.recordClick(dto, ip, userAgent);
+  async recordClick(): Promise<CallClick> {
+    return await this.callclickService.recordClick();
   }
 
   @Get()
